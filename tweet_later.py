608c6755ee9af88b580f6c3e23e0d1a3bd4a9ca0
ティン・ルーフ <tinruufu@gmail.com>
2016-09-06 16:03:55 +0100

    tweet later

---
+++ tweet_later.py
@@ -0,0 +1,12 @@
+from time import sleep
+from random import random
+
+
+def tweet_soon():
+    sleep(60 * 60 * random() * 6)
+    import tweet
+    tweet.tweet()
+
+
+if __name__ == '__main__':
+    tweet_soon()
